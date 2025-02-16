dbe79e44da2923dfd63b1fc6eb0fca2d4d24b0b9
evan <98708887@qq.com>
2025-02-16 20:14:50 +0800

    update

---
+++ LotteryDraw/LotteryDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotteryDrawApp.swift
+//  LotteryDraw
+//
+//  Created by evan on 2025/2/16.
+//
+
+import SwiftUI
+
+@main
+struct LotteryDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
